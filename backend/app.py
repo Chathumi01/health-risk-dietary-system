e3fcf2b5899b3badbe9f334cf0f3c86ec4da858d
Chathumi Gurugamage <chlgurugamage@students.nsbm.ac.lk>
2026-05-07 23:18:29 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -786,6 +786,7 @@ def predict():
         print("ERROR:", str(e))
         return jsonify({"error": "Server crash"}), 500
 
+app = Flask(__name__)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
